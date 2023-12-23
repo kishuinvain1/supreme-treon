64101657cad9182bfb262f0fe359c4dc6b19adaa
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-12-23 09:13:53 +0530

    commented out 49th line

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -46,7 +46,7 @@ def toRGB(image):
 def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     #img = Image.open(saved_image)
     #img = cv2.imread(saved_image)
-    img = cv2.cvtColor(saved_image,cv2.COLOR_BGR2RGB)
+    #img = cv2.cvtColor(saved_image,cv2.COLOR_BGR2RGB)
     x = int(x)
     y = int(y)
     w = int(w)
